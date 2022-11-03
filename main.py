baebe0ccc25775231a6ce62a3a2aeca6492f5a5c
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2022-11-03 08:08:04 +0300

    Separate misc and main

--- main.py
+++ main.py
@@ -1,62 +1,10 @@
 import yaml
-import os
-import shutil
 from env import GITHUB_USER, GITHUB_TOKEN
 import requests
 import datetime
+from misc import get_file_list, get_time_ago, copy_and_overwrite, inplace_change
 
 chains = ['kujira', 'stride', 'teritori', 'rebus']
-
-
-def display_time(seconds, granularity=1):
-    intervals = (
-        ('weeks', 604800),  # 60 * 60 * 24 * 7
-        ('days', 86400),  # 60 * 60 * 24
-        ('hours', 3600),  # 60 * 60
-        ('minutes', 60),
-        ('seconds', 1),
-    )
-
-    result = []
-
-    for name, count in intervals:
-        value = seconds // count
-        if value:
-            seconds -= value * count
-            if value == 1:
-                name = name.rstrip('s')
-            result.append("{} {}".format(round(value), name))
-
-    return ' '.join(result[:granularity]) + ' ago'
-
-
-def get_time_ago(start_time):
-    now = datetime.datetime.utcnow()
-    delta_in_seconds = ((now - start_time).total_seconds())
-    delta = display_time(abs(delta_in_seconds))
-
-    return delta
-
-
-def inplace_change(filename, old_string, new_string):
-    # Safely read the input filename using 'with'
-    with open(filename) as f:
-        s = f.read()
-        if old_string not in s:
-            print('"{old_string}" not found in {filename}.'.format(**locals()))
-            return
-
-    # Safely write the changed content, if found in the file
-    with open(filename, 'w') as f:
-        print('Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals()))
-        s = s.replace(old_string, new_string)
-        f.write(s)
-
-
-def copy_and_overwrite(from_path, to_path):
-    if os.path.exists(to_path):
-        shutil.rmtree(to_path)
-    shutil.copytree(from_path, to_path)
 
 
 def get_file_metadata(chain):
@@ -112,10 +60,6 @@ def get_replacement_params(chain):
         print("Request failed!")
 
     return replacements
-
-
-def get_file_list(path):
-    return [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
 
 def main():
